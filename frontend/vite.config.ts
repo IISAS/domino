b6eec1259773478acfd48b72b2950f089392f245
stevo <stefan.dlugolinsky@savba.sk>
2025-10-09 09:37:08 +0200

    parameterized base path in URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,32 @@
 import importMetaEnv from "@import-meta-env/unplugin";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    svgrPlugin(),
-    importMetaEnv.vite({ example: ".env.production" }),
-  ],
-  build: {
-    outDir: "build",
-  },
-  base: "/domino",
-});
+export default defineConfig(({ mode }) => {
+
+   // Load env file based on the current mode ('development' | 'production' etc.)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+    },
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+      importMetaEnv.vite({example: ".env.production"}),
+    ],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    build: {
+      outDir: "build",
+    },
+    base: env.BASENAME
+  }
+})
