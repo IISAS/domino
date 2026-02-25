8df2c56f87232c3bee70383b90f2bb55fde8197b
stevo <stefan.dlugolinsky@savba.sk>
2026-02-25 10:37:08 +0100

    routing update

--- frontend/src/routes/public.tsx
+++ frontend/src/routes/public.tsx
@@ -1,15 +1,8 @@
-import { lazyImport } from "@utils/lazyImports";
-import SignInPage from "@features/auth/pages/signIn/signInPage";
+  import SignInPage from "@features/auth/pages/signIn/signInPage";
 import SignUpPage from "@features/auth/pages/signUp/signUpPage";
-import { Navigate } from "react-router-dom";
-
-const { AuthRoutes } = lazyImport(
-  async () => await import("@features/auth/routes"),
-  "AuthRoutes",
-);
 
 export const publicRoutes = [
-  { path: "/sign-in", element: <SignInPage /> },
-  { path: "/sign-up", element: <SignUpPage /> },
+  { path: "sign-in", element: <SignInPage /> },
+  { path: "sign-up", element: <SignUpPage /> },
   // { path: "recover-password", element: <RecoverPasswordPage /> },
 ];
